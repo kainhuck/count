161596aa81e10855e92cd615a487a3cc56d2b1cf
huyukang <1628321264@qq.com>
2020-10-29 13:00:38 +0800

    add flag

--- setting/setting.go
+++ setting/setting.go
@@ -8,10 +8,25 @@ type Setting struct {
 }
 
 func NewDefault()*Setting{
+	return New(true, nil, false, false)
+}
+
+func New(ignoreHide bool, specifiedSuffix []string, ignoreSpaceLine bool, showInfo bool)*Setting{
 	return &Setting{
-		IgnoreHide:      true,
-		SpecifiedSuffix: []string{"go"},
-		IgnoreSpaceLine: true,
-		ShowInfo: true,
+		IgnoreHide:      ignoreHide,
+		SpecifiedSuffix: specifiedSuffix,
+		IgnoreSpaceLine: ignoreSpaceLine,
+		ShowInfo:        showInfo,
 	}
+}
+
+func NewDefaultBySuffix(suffix []string) *Setting{
+	return New(true, suffix, false, false)
+}
+
+func (s *Setting)AddSuffix(suffix string){
+	if s.SpecifiedSuffix == nil{
+		s.SpecifiedSuffix = make([]string, 0, 1)
+	}
+	s.SpecifiedSuffix = append(s.SpecifiedSuffix)
 }
